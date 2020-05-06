bca3b946aaedd2bbe253058963e80bce1e4b1249
Ross Maughan <rmaughan@ibm.com>
2020-05-06 00:42:31 -0400

    Added function for removing dupes in results

--- libs/core/src/util/general.ts
+++ libs/core/src/util/general.ts
@@ -1,3 +1,4 @@
+import { IDaodResults } from "../service";
 
 export class UtilGeneral {
 
@@ -29,5 +30,20 @@ export class UtilGeneral {
         return dateTime.replace(/Z/g, '');
     }
 
+
+    /**
+     * Remove all duplicate items from the entity and links within a result set based on unique item ids
+     */
+    public static removeDuplicates(results: IDaodResults): IDaodResults {
+        let seen = {};
+        results.entities = results.entities.filter(item => {
+            return seen.hasOwnProperty(item.id.toString()) ? false : (seen[item.id.toString()] = true);
+        });
+        seen = {};
+        results.links = results.links.filter(item => {
+            return seen.hasOwnProperty(item.id.toString()) ? false : (seen[item.id.toString()] = true);
+        });
+        return results;
+    }    
 
 }
